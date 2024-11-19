3eb26a8c8a9615ffd7ea8d83c32b56791c254d9d
Sonzzini <paulo.sonzzini@gmail.com>
2024-11-19 13:42:17 -0300

    Initial Commit

---
+++ Desafio_TechAcme_ViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Desafio_TechAcme_ViewCode
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 19/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Desafio_TechAcme_ViewCodeTests/Desafio_TechAcme_ViewCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_TechAcme_ViewCodeTests.swift
+//  Desafio_TechAcme_ViewCodeTests
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 19/11/24.
+//
+
+import Testing
+@testable import Desafio_TechAcme_ViewCode
+
+struct Desafio_TechAcme_ViewCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
